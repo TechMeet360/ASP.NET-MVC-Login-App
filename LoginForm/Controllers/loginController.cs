a23432f19cd5f1d5a63b0f3b7c51d34646381299
Suhas Parameshwara <Suhas.Parameshwara@biztalk360.co.uk>
2019-02-17 13:41:32 +0530

    Add project files.

---
+++ LoginForm/Controllers/loginController.cs
@@ -0,0 +1,37 @@
+﻿using LoginForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: login
+        static List<User> sun = new List<User>();
+        public ActionResult Index()
+        {
+            return View(sun);
+        }
+        public ActionResult Record(User sun)
+        {
+            return View(sun);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Login(User sonu)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", sonu);
+            }
+            sun.Add(sonu);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LoginForm/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginForm.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Passward { get; set; }
+        public string Email { get; set; }
+    }
+}
